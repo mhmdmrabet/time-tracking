7e8800aee16b305b5197995688d298831781194e
Mohamed <mhmdmrabet@yahoo.fr>
2022-01-27 10:45:46 +0100

    First approach - Methods

--- app.js
+++ app.js
@@ -4,7 +4,30 @@ Vue.createApp({
       title: "Time Tracking",
       logoSource: "https://cdn.svgporn.com/logos/vue.svg",
       taskName: "",
-      count: 0,
+      isTaskInProgress: false,
+      startTime: null,
+      errorMsg: null,
     };
+  },
+  methods: {
+    startTask() {
+      // Verify
+      if (!this.taskName) {
+        this.errorMsg = "Task name cannot be empty.";
+        return;
+      } else if (this.isTaskInProgress) {
+        this.errorMsg = "A task is already in progress.";
+        return;
+      } else {
+        this.errorMsg = null;
+      }
+      // Task
+      this.isTaskInProgress = true;
+      this.startTime = Date.now();
+    },
+    stopTask() {
+      this.isTaskInProgress = false;
+      this.errorMsg = null;
+    },
   },
 }).mount("#app");
